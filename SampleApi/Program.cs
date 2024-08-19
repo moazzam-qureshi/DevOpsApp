956da740ef2d7eac98399cabb5383b516bc1300a
Moazzam <technocares.moazzam@gmail.com>
2024-08-19 10:02:09 +0500

    test pipeline change

--- SampleApi/Program.cs
+++ SampleApi/Program.cs
@@ -9,12 +9,12 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{ 
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 var summaries = new[]
 {
